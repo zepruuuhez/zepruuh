65ac7c7f66e6575e81b828846150e5ba30234dc0
zepruuuhez <57395202+zepruuuhez@users.noreply.github.com>
2020-02-06 18:42:48 +0700

    part 14
    fgfhfh

--- zepruuh/fifteen.Designer.cs
+++ zepruuh/fifteen.Designer.cs
@@ -44,6 +44,7 @@
             this.label14 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
             this.label16 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -74,7 +75,7 @@
             // 
             this.label3.BackColor = System.Drawing.Color.DarkSlateGray;
             this.label3.ForeColor = System.Drawing.Color.Silver;
-            this.label3.Location = new System.Drawing.Point(10, 70);
+            this.label3.Location = new System.Drawing.Point(130, 10);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(50, 50);
             this.label3.TabIndex = 2;
@@ -86,7 +87,7 @@
             // 
             this.label4.BackColor = System.Drawing.Color.DarkSlateGray;
             this.label4.ForeColor = System.Drawing.Color.Silver;
-            this.label4.Location = new System.Drawing.Point(70, 70);
+            this.label4.Location = new System.Drawing.Point(190, 70);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(50, 50);
             this.label4.TabIndex = 3;
@@ -98,7 +99,7 @@
             // 
             this.label5.BackColor = System.Drawing.Color.DarkSlateGray;
             this.label5.ForeColor = System.Drawing.Color.Silver;
-            this.label5.Location = new System.Drawing.Point(130, 10);
+            this.label5.Location = new System.Drawing.Point(10, 70);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(50, 50);
             this.label5.TabIndex = 4;
@@ -110,7 +111,7 @@
             // 
             this.label6.BackColor = System.Drawing.Color.DarkSlateGray;
             this.label6.ForeColor = System.Drawing.Color.Silver;
-            this.label6.Location = new System.Drawing.Point(130, 70);
+            this.label6.Location = new System.Drawing.Point(70, 70);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(50, 50);
             this.label6.TabIndex = 5;
@@ -122,7 +123,7 @@
             // 
             this.label7.BackColor = System.Drawing.Color.DarkSlateGray;
             this.label7.ForeColor = System.Drawing.Color.Silver;
-            this.label7.Location = new System.Drawing.Point(190, 10);
+            this.label7.Location = new System.Drawing.Point(130, 70);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(50, 50);
             this.label7.TabIndex = 6;
@@ -134,7 +135,7 @@
             // 
             this.label8.BackColor = System.Drawing.Color.DarkSlateGray;
             this.label8.ForeColor = System.Drawing.Color.Silver;
-            this.label8.Location = new System.Drawing.Point(190, 70);
+            this.label8.Location = new System.Drawing.Point(190, 130);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(50, 50);
             this.label8.TabIndex = 7;
@@ -182,7 +183,7 @@
             // 
             this.label12.BackColor = System.Drawing.Color.DarkSlateGray;
             this.label12.ForeColor = System.Drawing.Color.Silver;
-            this.label12.Location = new System.Drawing.Point(190, 130);
+            this.label12.Location = new System.Drawing.Point(190, 190);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(50, 50);
             this.label12.TabIndex = 11;
@@ -228,13 +229,23 @@
             // 
             // label16
             // 
-            this.label16.BackColor = System.Drawing.Color.Teal;
+            this.label16.BackColor = System.Drawing.Color.Transparent;
             this.label16.ForeColor = System.Drawing.Color.Silver;
-            this.label16.Location = new System.Drawing.Point(190, 190);
+            this.label16.Location = new System.Drawing.Point(190, 10);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(50, 50);
             this.label16.TabIndex = 15;
             this.label16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(279, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(230, 50);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Перемешать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // fifteen
             // 
@@ -242,6 +253,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(927, 506);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label16);
             this.Controls.Add(this.label15);
             this.Controls.Add(this.label14);
@@ -259,7 +271,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F);
-            this.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.Margin = new System.Windows.Forms.Padding(8);
             this.Name = "fifteen";
             this.Text = "fifteen";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.fifteen_FormClosing);
@@ -285,5 +297,6 @@
         private System.Windows.Forms.Label label14;
         private System.Windows.Forms.Label label15;
         private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Button button1;
     }
 }
--- zepruuh/fifteen.cs
+++ zepruuh/fifteen.cs
@@ -22,11 +22,81 @@ namespace zepruuh
             Form back = Application.OpenForms[1];
             back.Show();
         }
+        public void win()
+        {
+            int lbx = 10, lby = 10, lbcount = 1;
+            bool IsWon = true;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Label lb = Controls["label" + lbcount] as Label;
+                    if (lb.Location.X != lbx || lb.Location.Y != lby)
+                    {
+                        IsWon = false;
+                    }
+                    lbx += 60;
+                    lbcount++;
+                }
+                lby += 60;
+                lbx = 10;
+            }
+            if (IsWon)
+                MessageBox.Show("Вы победили");
+        }
 
         private void label_click(object sender, EventArgs e)
         {
             Label lb = sender as Label;
-            MessageBox.Show(lb.Name);
+            int lbX = lb.Location.X,
+                lbY = lb.Location.Y;
+            if (label16.Location.X == lbX + 60 && label16.Location.Y == lbY)
+            {
+                lb.Location = new Point(lbX + 60,lbY);
+                label16.Location = new Point(lbX , lbY);
+            }
+            else if (label16.Location.X == lbX - 60 && label16.Location.Y == lbY)
+            {
+                lb.Location = new Point(lbX - 60, lbY);
+                label16.Location = new Point(lbX, lbY);
+            }
+            else if (label16.Location.X == lbX && label16.Location.Y == lbY + 60)
+            {
+                lb.Location = new Point(lbX , lbY + 60);
+                label16.Location = new Point(lbX, lbY);
+            }
+            else if (label16.Location.X == lbX && label16.Location.Y == lbY - 60)
+            {
+                lb.Location = new Point(lbX, lbY - 60);
+                label16.Location = new Point(lbX, lbY);
+            }
+            win();
+        }
+        
+        private void AssignIconsToSquares()
+        {
+            List<string> icons = new List<string>()
+            {
+                "1", "2", "3", "4", "5", "6", "7", "8",
+                "9", "10", "11", "12", "13", "14", "15", "16"
+            };
+            Random random = new Random();
+            // Контейнер имеет 16 ячеек,  лист имеет 16 иконок. 
+            //Иконки рандомно выбираются из листа и добавляются в каждый Label
+            foreach (Control control in this.Controls) //создаем переменную control, которая хранит в себе каждый элемент управления
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber]; //скрываем иконки (изменяем их цвет на цвет фона)
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AssignIconsToSquares();
         }
     }
 }
